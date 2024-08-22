c5d615c31c2969534f5814de8acec9e0d429feb8
Muhammad Yahya <realrockyahya@gmail.com>
2024-08-22 23:22:52 +0500

    defaultThemeLight

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,23 +1,23 @@
 // ThemeContext.js
 
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext } from "react";
 
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('dark');
-  const [next,setnext]=useState(0);
+  const [theme, setTheme] = useState("light");
+  const [next, setnext] = useState(0);
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
-  const  CheckNext=()=>{
-    setnext((prev)=>(prev+1));  
-    next>2&&setnext(0)
-  }
+  const CheckNext = () => {
+    setnext((prev) => prev + 1);
+    next > 2 && setnext(0);
+  };
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme ,next,CheckNext}}>
+    <ThemeContext.Provider value={{ theme, toggleTheme, next, CheckNext }}>
       {children}
     </ThemeContext.Provider>
   );
