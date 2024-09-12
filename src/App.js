7b849d2953f2bd4170d36cd89e6d456a7e07d1b5
Muhammad Yahya <realrockyahya@gmail.com>
2024-09-12 23:11:50 +0500

    aicommitterakianamrkhon

--- src/App.js
+++ src/App.js
@@ -1,57 +1,59 @@
 // App.js
 
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useTheme } from './context/ThemeContext';
-import { SignupRider } from './Pages/Rider/SignupRider';
-import { LoginRider } from './Pages/Rider/LoginRider';
-import { WorkingAreas } from './Pages/Rider/WorkingAreas';
-import { MyRides } from './Pages/Rider/MyRides';
-import NewListing from './Pages/Restaurant/NewListing';
-import History from './Pages/Rider/History';
-import Signup from './Pages/Customer/Signup';
-import SignupRestaurant from './Pages/Restaurant/Signup';
-import LoginRestaurant from './Pages/Restaurant/Login';
-import HomeRestaurant from './Pages/Restaurant/HomeRestaurant';
-import HomeCustomer from './Pages/Customer/HomeCustomer';
-import LoginCustomer from './Pages/Customer/LoginCustomer';
-import BigByte from './Pages/BigByte';
-import SeeDetails from './Pages/Restaurant/components/SeeDetails';
-import PlansandPricing from './Pages/Restaurant/PlansandPricing';
-import ManageList from './Pages/Restaurant/ManageList';
-import Read from './Pages/Restaurant/Read';
-import Edit from './Pages/Restaurant/Edit';
-import Restaurant from './Pages/Customer/component/Restaurant';
-import '@shoelace-style/shoelace/dist/themes/light.css';
-import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
-import MyCart from './Pages/MyCart';
-import Checkout from './Pages/Customer/Checkout';
-import MyOrders from './Pages/Customer/MyOrders';
-import Schedule from './Pages/Customer/Schedule';
-import DailyScheduler from './Pages/Customer/DailyScheduler';
-import WorkingZonePage from './Pages/Rider/WorkingZonePage';
-import MyStatus from './Pages/Restaurant/MyStatus';
-import TrackRider from './Pages/Restaurant/trackRider';
-import Favourites from './Pages/Customer/Favourites';
-import BigRides from './Pages/Rider/BigRides';
-import UpdateDisease from './Pages/Customer/UpdateDisease';
-import CustomerHistory from './Pages/Customer/CustomerHistory';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useTheme } from "./context/ThemeContext";
+import { SignupRider } from "./Pages/Rider/SignupRider";
+import { LoginRider } from "./Pages/Rider/LoginRider";
+import { WorkingAreas } from "./Pages/Rider/WorkingAreas";
+import { MyRides } from "./Pages/Rider/MyRides";
+import NewListing from "./Pages/Restaurant/NewListing";
+import History from "./Pages/Rider/History";
+import Signup from "./Pages/Customer/Signup";
+import SignupRestaurant from "./Pages/Restaurant/Signup";
+import LoginRestaurant from "./Pages/Restaurant/Login";
+import HomeRestaurant from "./Pages/Restaurant/HomeRestaurant";
+import HomeCustomer from "./Pages/Customer/HomeCustomer";
+import LoginCustomer from "./Pages/Customer/LoginCustomer";
+import BigByte from "./Pages/BigByte";
+import SeeDetails from "./Pages/Restaurant/components/SeeDetails";
+import PlansandPricing from "./Pages/Restaurant/PlansandPricing";
+import ManageList from "./Pages/Restaurant/ManageList";
+import Read from "./Pages/Restaurant/Read";
+import Edit from "./Pages/Restaurant/Edit";
+import Restaurant from "./Pages/Customer/component/Restaurant";
+import "@shoelace-style/shoelace/dist/themes/light.css";
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path";
+import MyCart from "./Pages/MyCart";
+import Checkout from "./Pages/Customer/Checkout";
+import MyOrders from "./Pages/Customer/MyOrders";
+import Schedule from "./Pages/Customer/Schedule";
+import DailyScheduler from "./Pages/Customer/DailyScheduler";
+import WorkingZonePage from "./Pages/Rider/WorkingZonePage";
+import MyStatus from "./Pages/Restaurant/MyStatus";
+import TrackRider from "./Pages/Restaurant/trackRider";
+import Favourites from "./Pages/Customer/Favourites";
+import BigRides from "./Pages/Rider/BigRides";
+import UpdateDisease from "./Pages/Customer/UpdateDisease";
+import CustomerHistory from "./Pages/Customer/CustomerHistory";
+import RestaurantActiveStatus from "./Pages/Restaurant/RestaurantActiveStatus";
 
-setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.0/cdn/');
+setBasePath(
+  "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.0/cdn/"
+);
 
 function App() {
   const { theme, toggleTheme } = useTheme();
 
   const appStyles = {
-    backgroundColor: theme === 'light' ? 'white' : '#292929',
-    color: theme === 'light' ? 'black' : 'white',
-    minHeight: '100vh',
-    transition: 'all 0.3s ease',
+    backgroundColor: theme === "light" ? "white" : "#292929",
+    color: theme === "light" ? "black" : "white",
+    minHeight: "100vh",
+    transition: "all 0.3s ease",
   };
 
   return (
     <div style={appStyles}>
-     
       <BrowserRouter>
         <Routes>
           <Route exact path="/WorkingZonePage" Component={WorkingZonePage} />
@@ -85,8 +87,12 @@ function App() {
           <Route exact path="/customer/myOrders" Component={MyOrders} />
           <Route exact path="/customer/schedule" Component={Schedule} />
           <Route exact path="/customer/daily" Component={DailyScheduler} />
-          <Route exact path="/customer/MyOrderHistory" Component={CustomerHistory} />
-          
+          <Route
+            exact
+            path="/customer/MyOrderHistory"
+            Component={CustomerHistory}
+          />
+          <Route exact path="/MyStatus" Component={RestaurantActiveStatus} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Pages/Restaurant/RestaurantActiveStatus.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { NavbarHome } from "./components/NavbarHome";
+import { Card, Row, Col } from "react-bootstrap";
+
+export default function RestaurantActiveStatus() {
+  const [fromDate, setFromDate] = useState(
+    () => new Date().toISOString().slice(0, 10) // Slices out the YYYY-MM-DD format
+  );
+
+  function adjustDate(date, days) {
+    const adjustedDate = new Date(date); // Create a new Date object to avoid modifying the original date
+    adjustedDate.setDate(adjustedDate.getDate() + days); // Add or subtract days
+    return adjustedDate.toISOString().slice(0, 10); // Format as YYYY-MM-DD
+  }
+
+  const [toDate, setToDate] = useState(adjustDate(fromDate, 1));
+
+  const handleFromDate = (e) => {
+    setFromDate(e.target.value);
+  };
+
+  const handleToDate = (e) => {
+    setToDate(e.target.value);
+  };
+
+  return (
+    <>
+      <NavbarHome />
+      <div className="container p-4">
+        <Row className="justify-content-center">
+          <Col xs={12} sm={10} md={8} lg={6}>
+            <Card className="shadow-sm">
+              <Card.Header className="bg-warning text-primary fs-4 text-center">
+                Set Unavailability
+              </Card.Header>
+              <Card.Body>
+                <Row className="align-items-center mb-3">
+                  <Col xs={12} sm={4}>
+                    <Card.Title>From</Card.Title>
+                  </Col>
+                  <Col xs={12} sm={8}>
+                    <input
+                      type="date"
+                      className="form-control"
+                      value={fromDate}
+                      onChange={handleFromDate}
+                    />
+                  </Col>
+                </Row>
+                <Row className="align-items-center">
+                  <Col xs={12} sm={4}>
+                    <Card.Title>To</Card.Title>
+                  </Col>
+                  <Col xs={12} sm={8}>
+                    <input
+                      type="date"
+                      className="form-control"
+                      value={toDate}
+                      onChange={handleToDate}
+                    />
+                  </Col>
+                </Row>
+              </Card.Body>
+              <Card.Footer className="text-center">
+                <button className="btn btn-danger w-100 rounded">Update</button>
+              </Card.Footer>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+}
